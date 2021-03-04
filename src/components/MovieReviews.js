8e7d465f5b5c492c3b7119c274b6c0c27d6e0de9
ajkelso <ajkelso@gmail.com>
2021-03-04 14:17:55 -0500

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,30 @@
 // Code MovieReviews Here
+import React from "react";
+
+
+
+const Review = (review) => {
+    return (
+        <div 
+            className="review"
+            key={review.headline} >
+                {review.headline}
+        </div>
+    )
+}
+
+const MovieReviews = (props) => {
+    return <div className="review-list">
+        <h3>{props.reviews && props.reviews.length ? props.category : null }</h3>
+        <ul>
+            {props.reviews.map(review => Review(review))}
+        </ul>
+    </div>
+}
+
+MovieReviews.defaultProps = {
+    reviews: []
+}
+
+export default MovieReviews
+
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -6,4 +6,47 @@ const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code SearchableMovieReviewsContainer Here
+export default class SearchableMovieReviewsContainer extends Component {
+    
+    state = {
+        reviews: [],
+        searchTerm: ""
+    }
+    
+    url = (term) => "https://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ&query=" + term
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        fetch(this.url(this.state.searchTerm))
+        .then(res => res.json())
+        .then(data => this.setState({reviews: data.results}))
+
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            searchTerm: event.target.value
+        })
+    }
+
+    renderResults = () => {
+
+        if(this.state.reviews === null){
+            return <h4>No Results Found</h4>
+        } else {
+            return <MovieReviews category="Search Results" reviews={this.state.reviews} />
+        }
+    }
+    
+    render(){
+        return(
+            <div className="searchable-movie-reviews">
+                <form onSubmit={this.handleSubmit}>
+                    <label id="search">Search Reviews: </label>
+                    <input onChange={this.handleChange} type="text" id="search" value={this.state.searchTerm}></input>
+                    {this.renderResults()}
+                </form>
+            </div>
+        )
+    }
+}
